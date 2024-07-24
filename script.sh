ceba4e7d64cc9ecee6e406b4888e566176f77614
sandeepreddy9909 <saisandeepreddy3010@gmail.com>
2024-07-24 05:04:04 +0000

    script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-echo "updating system"
-sudo apt update -y 
+echo "Updating System"
+sudo apt update -y
 
-echo "Installing utilities"
-sudo apt install -y zip unzip
+echo "Installing Utilities"
+sudo apt install -y zip unzip 
 
-echo "echo NGINX web server"
-sudo apt install -y ngnix
+echo "Installing NGINX Web Server"
+sudo apt install -y nginx
 
-echo "remove sample page "
+echo "Remove Sample Pages"
 sudo rm -rf /var/www/html
 
-echo "clone login app"
-sudo git clone https://github.com/sandeepreddy9909s/login-page-2426.git var/www/html
+echo "Clone Login App"
+sudo git clone https://github.com/sandeepreddy9909s/login-page-2426.git /var/www/html
 
-echo "script execution completed"
+
+
+
+
+
+
